882537ff9ddca5bf4ba6247ab31b376a35981c31
Nyipoji Kani Jibarang <126312255+01nyipoji@users.noreply.github.com>
2024-04-05 22:07:45 +0100

    updated objects assessment

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const score = 89;
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke&quot"
+    };
 
-let course = '';
+    //a
+    programming.languages.push('Go');
+    
+    //b
+    programming.difficulty = 8;
+    
+    //c
+     delete programming.jokes;
 
-if(score >= 90){
-    course = 'Computer Science';
-} else if(score >= 80){
-    course = 'Mechanical Engineering';
-} else if(score >= 70){
-    course = 'Chemical Engineering';
-} else if(score >= 60){
-    course = 'Electronics and communication';
-} else if(score >= 50){
-    course = 'Civil Engineering';
-} else {
-    course = 'no admission';
-}
+     //d 
+     programming.isFun = true;
+     
+     //e
+     for (let i = 0; i < programming.languages.length; i++){
+        console.log(programming.languages[i]);
+     };
+    
+     //f
+     for (let key in programming){
+        console.log(key);
+     };
 
-console.log(`You've been offered ${course} `);
+     //g
+     for (let key in programming){
+        console.log(programming[key]);
+     };
