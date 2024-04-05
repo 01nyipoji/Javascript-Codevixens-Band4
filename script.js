f18a0eded0b1dbcfb35de28d3c715c615945d67e
Nyipoji Kani Jibarang <126312255+01nyipoji@users.noreply.github.com>
2024-04-05 21:34:38 +0100

    Switch assessment

---
+++ script.js
@@ -0,0 +1,19 @@
+const score = 89;
+
+let course = '';
+
+if(score >= 90){
+    course = 'Computer Science';
+} else if(score >= 80){
+    course = 'Mechanical Engineering';
+} else if(score >= 70){
+    course = 'Chemical Engineering';
+} else if(score >= 60){
+    course = 'Electronics and communication';
+} else if(score >= 50){
+    course = 'Civil Engineering';
+} else {
+    course = 'no admission';
+}
+
+console.log(`You've been offered ${course} `);
